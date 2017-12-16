7a8f8712bc069ea14a1288239e694800f0de828c
hongjunwang <hongjun.developer@gmail.com>
2017-12-16 22:18:46 +0800

    Updated to Swift 4
    Updated to Swift 4

--- tunnel_server/UDPServerConnection.swift
+++ tunnel_server/UDPServerConnection.swift
@@ -14,11 +14,11 @@ class UDPServerConnection: Connection {
 
 	// MARK: Properties
 
-	/// The address family of the UDP socket.
+    /// The address family of the UDP socket.
     var addressFamily: Int32 = AF_UNSPEC
 
-	/// A dispatch source for reading data from the UDP socket.
-    var responseSource: dispatch_source_t?
+    /// A dispatch source for reading data from the UDP socket.
+    var responseSource: DispatchSourceRead?
 
 	// MARK: Initializers
     
@@ -27,41 +27,47 @@ class UDPServerConnection: Connection {
     }
     
     deinit {
-		if responseSource != nil {
-			dispatch_source_cancel(responseSource!)
-		}
+        responseSource?.cancel()
     }
 
 	// MARK: Interface
 
-	/// Convert a sockaddr structure into an IP address string and port.
-    func getEndpointFromSocketAddress(socketAddressPointer: UnsafePointer<sockaddr>) -> (host: String, port: Int)? {
-		let socketAddress = UnsafePointer<sockaddr>(socketAddressPointer).memory
+    /// Convert a sockaddr structure into an IP address string and port.
+    func getEndpointFromSocketAddress(_ socketAddressPointer: UnsafePointer<sockaddr>) -> (host: String, port: Int)? {
+        let saFamily = socketAddressPointer.pointee.sa_family
 
-		switch Int32(socketAddress.sa_family) {
-			case AF_INET:
-				var socketAddressInet = UnsafePointer<sockaddr_in>(socketAddressPointer).memory
-				let length = Int(INET_ADDRSTRLEN) + 2
-				var buffer = [CChar](count: length, repeatedValue: 0)
-				let hostCString = inet_ntop(AF_INET, &socketAddressInet.sin_addr, &buffer, socklen_t(length))
-				let port = Int(UInt16(socketAddressInet.sin_port).byteSwapped)
-				return (String.fromCString(hostCString)!, port)
-
-			case AF_INET6:
-				var socketAddressInet6 = UnsafePointer<sockaddr_in6>(socketAddressPointer).memory
-				let length = Int(INET6_ADDRSTRLEN) + 2
-				var buffer = [CChar](count: length, repeatedValue: 0)
-				let hostCString = inet_ntop(AF_INET6, &socketAddressInet6.sin6_addr, &buffer, socklen_t(length))
-				let port = Int(UInt16(socketAddressInet6.sin6_port).byteSwapped)
-				return (String.fromCString(hostCString)!, port)
-
-			default:
-				return nil
+		switch Int32(saFamily) {
+        case AF_INET:
+            let endpoint = socketAddressPointer.withMemoryRebound(to: sockaddr_in.self, capacity: 1) { pointer -> (host: String, port: Int) in
+                var socketAddressInet = pointer.pointee
+                let length = Int(INET_ADDRSTRLEN) + 2
+                var buffer = [CChar](repeating: 0, count: length)
+                let hostCString = inet_ntop(AF_INET, &socketAddressInet.sin_addr, &buffer, socklen_t(length))
+                let port = Int(UInt16(socketAddressInet.sin_port).byteSwapped)
+                
+                return (String(cString: hostCString!), port)
+            }
+            return endpoint
+            
+        case AF_INET6:
+            let endpoint = socketAddressPointer.withMemoryRebound(to: sockaddr_in6.self, capacity: 1) { pointer -> (host: String, port: Int) in
+                var socketAddressInet6 = pointer.pointee
+                let length = Int(INET6_ADDRSTRLEN) + 2
+                var buffer = [CChar](repeating: 0, count: length)
+                let hostCString = inet_ntop(AF_INET6, &socketAddressInet6.sin6_addr, &buffer, socklen_t(length))
+                let port = Int(UInt16(socketAddressInet6.sin6_port).byteSwapped)
+                
+                return (String(cString: hostCString!), port)
+            }
+            return endpoint
+            
+        default:
+            return nil
 		}
     }
 
     /// Create a UDP socket
-    func createSocketWithAddressFamilyFromAddress(address: String) -> Bool {
+    func createSocketWithAddressFamilyFromAddress(_ address: String) -> Bool {
 		var sin = sockaddr_in()
 		var sin6 = sockaddr_in6()
 		var newSocket: Int32 = -1
@@ -78,63 +84,62 @@ class UDPServerConnection: Connection {
 		}
 
 		guard newSocket > 0 else { return false }
+        
+        let newResponseSource = DispatchSource.makeReadSource(fileDescriptor: newSocket, queue: DispatchQueue.main)
+        newResponseSource.setCancelHandler {
+            simpleTunnelLog("closing udp socket for connection \(self.identifier)")
+            let UDPSocket = Int32(newResponseSource.handle)
+            close(UDPSocket)
+        }
 
-		guard let newResponseSource = dispatch_source_create(DISPATCH_SOURCE_TYPE_READ, UInt(newSocket), 0, dispatch_get_main_queue()) else {
-			close(newSocket)
-			return false
-		}
-
-		dispatch_source_set_cancel_handler(newResponseSource) {
-			simpleTunnelLog("closing udp socket for connection \(self.identifier)")
-			let UDPSocket = Int32(dispatch_source_get_handle(newResponseSource))
-			close(UDPSocket)
-		}
+        newResponseSource.setEventHandler {
+            guard let source = self.responseSource else { return }
             
-		dispatch_source_set_event_handler(newResponseSource) {
-			guard let source = self.responseSource else { return }
+            var socketAddress = sockaddr_storage()
+            var socketAddressLength = socklen_t(MemoryLayout<sockaddr_storage>.size)
+            var response = [UInt8](repeating: 0, count: 4096)
+            let UDPSocket = Int32(source.handle)
+            
+            let bytesRead = withUnsafeMutablePointer(to: &socketAddress) {
+                $0.withMemoryRebound(to: sockaddr.self, capacity: 1) {
+                    recvfrom(UDPSocket, &response, response.count, 0, $0, &socketAddressLength)
+                }
+            }
+            
+            guard bytesRead >= 0 else {
+                simpleTunnelLog("recvfrom failed: \(String(cString: strerror(errno)))")
+                self.closeConnection(.all)
+                return
+            }
+            
+            guard bytesRead > 0 else {
+                simpleTunnelLog("recvfrom returned EOF")
+                self.closeConnection(.all)
+                return
+            }
+            
+            guard let endpoint = withUnsafePointer(to: &socketAddress, {
+                $0.withMemoryRebound(to: sockaddr.self, capacity: 1) { self.getEndpointFromSocketAddress($0) }
+            })
+            else {
+                simpleTunnelLog("Failed to get the address and port from the socket address received from recvfrom")
+                self.closeConnection(.all)
+                return
+            }
+            
+            let responseDatagram = Data(bytes: &response, count: bytesRead)
+            simpleTunnelLog("UDP connection id \(self.identifier) received = \(bytesRead) bytes from host = \(endpoint.host) port = \(endpoint.port)")
+            self.tunnel?.sendDataWithEndpoint(responseDatagram, forConnection: self.identifier, host: endpoint.host, port: endpoint.port)
+        }
 
-			var socketAddress = sockaddr_storage()
-			var socketAddressLength = socklen_t(sizeof(sockaddr_storage.self))
-			let response = [UInt8](count: 4096, repeatedValue: 0)
-			let UDPSocket = Int32(dispatch_source_get_handle(source))
-
-			let bytesRead = withUnsafeMutablePointer(&socketAddress) {
-				recvfrom(UDPSocket, UnsafeMutablePointer<Void>(response), response.count, 0, UnsafeMutablePointer($0), &socketAddressLength)
-			}
-
-			guard bytesRead >= 0 else {
-				if let errorString = String(UTF8String: strerror(errno)) {
-					simpleTunnelLog("recvfrom failed: \(errorString)")
-				}
-				self.closeConnection(.All)
-				return
-			}
-
-			guard bytesRead > 0 else {
-				simpleTunnelLog("recvfrom returned EOF")
-				self.closeConnection(.All)
-				return
-			}
-
-			guard let endpoint = withUnsafePointer(&socketAddress, { self.getEndpointFromSocketAddress(UnsafePointer($0)) }) else {
-				simpleTunnelLog("Failed to get the address and port from the socket address received from recvfrom")
-				self.closeConnection(.All)
-				return
-			}
-
-			let responseDatagram = NSData(bytes: UnsafePointer<Void>(response), length: bytesRead)
-			simpleTunnelLog("UDP connection id \(self.identifier) received = \(bytesRead) bytes from host = \(endpoint.host) port = \(endpoint.port)")
-			self.tunnel?.sendDataWithEndPoint(responseDatagram, forConnection: self.identifier, host: endpoint.host, port: endpoint.port)
-		}
-
-		dispatch_resume(newResponseSource)
+        newResponseSource.resume()
 		responseSource = newResponseSource
 
 		return true
     }
 
     /// Send a datagram to a given host and port.
-    override func sendDataWithEndPoint(data: NSData, host: String, port: Int) {
+    override func sendDataWithEndPoint(_ data: Data, host: String, port: Int) {
 
 		if responseSource == nil {
 			guard createSocketWithAddressFamilyFromAddress(host) else {
@@ -144,7 +149,7 @@ class UDPServerConnection: Connection {
 		}
 
 		guard let source = responseSource else { return }
-		let UDPSocket = Int32(dispatch_source_get_handle(source))
+		let UDPSocket = Int32(source.handle)
 		let sent: Int
 
 		switch addressFamily {
@@ -156,8 +161,10 @@ class UDPServerConnection: Connection {
 				}
 				serverAddress.setPort(port)
 
-				sent = withUnsafePointer(&serverAddress.sin) {
-					sendto(UDPSocket, data.bytes, data.length, 0, UnsafePointer($0), socklen_t(serverAddress.sin.sin_len))
+                sent = withUnsafePointer(to: &serverAddress.sin) {
+                    $0.withMemoryRebound(to: sockaddr.self, capacity: 1) {
+                        sendto(UDPSocket, (data as NSData).bytes, data.count, 0, UnsafePointer($0), socklen_t(serverAddress.sin.sin_len))
+                    }
 				}
 
 			case AF_INET6:
@@ -168,8 +175,10 @@ class UDPServerConnection: Connection {
 				}
 				serverAddress.setPort(port)
 
-				sent = withUnsafePointer(&serverAddress.sin6) {
-					sendto(UDPSocket, data.bytes, data.length, 0, UnsafePointer($0), socklen_t(serverAddress.sin6.sin6_len))
+                sent = withUnsafePointer(to: &serverAddress.sin6) {
+                    $0.withMemoryRebound(to: sockaddr.self, capacity: 1) {
+                        sendto(UDPSocket, (data as NSData).bytes, data.count, 0, UnsafePointer($0), socklen_t(serverAddress.sin6.sin6_len))
+                    }
 				}
 
 			default:
@@ -177,25 +186,25 @@ class UDPServerConnection: Connection {
         }
 
 		guard sent > 0 else {
-			if let errorString = String(UTF8String: strerror(errno)) {
+			if let errorString = String(utf8String: strerror(errno)) {
 				simpleTunnelLog("UDP connection id \(identifier) failed to send data to host = \(host) port \(port). error = \(errorString)")
 			}
-			closeConnection(.All)
+			closeConnection(.all)
 			return
 		}
 
-		if sent == data.length {
+		if sent == data.count {
 			// Success
-			simpleTunnelLog("UDP connection id \(identifier) sent \(data.length) bytes to host = \(host) port \(port)")
+			simpleTunnelLog("UDP connection id \(identifier) sent \(data.count) bytes to host = \(host) port \(port)")
 		}
     }
 
-	/// Close the connection.
-	override func closeConnection(direction: TunnelConnectionCloseDirection) {
+    /// Close the connection.
+	override func closeConnection(_ direction: TunnelConnectionCloseDirection) {
 		super.closeConnection(direction)
 
-		if let source = responseSource where isClosedForWrite && isClosedForRead {
-			dispatch_source_cancel(source)
+		if let source = responseSource, isClosedForWrite && isClosedForRead {
+            source.cancel()
 			responseSource = nil
 		}
 	}
