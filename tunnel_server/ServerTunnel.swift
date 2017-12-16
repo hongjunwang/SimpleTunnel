a89dd7c97b5329f105304af68ec1cc490e8af550
hongjunwang <hongjun.developer@gmail.com>
2017-12-16 17:30:49 +0800

    Updated to Swift 4
    ServerTunnel.swift

--- tunnel_server/ServerTunnel.swift
+++ tunnel_server/ServerTunnel.swift
@@ -10,15 +10,15 @@ import Foundation
 import SystemConfiguration
 
 /// The server-side implementation of the SimpleTunnel protocol.
-class ServerTunnel: Tunnel, TunnelDelegate, NSStreamDelegate {
+class ServerTunnel: Tunnel, TunnelDelegate, StreamDelegate {
 
 	// MARK: Properties
 
 	/// The stream used to read data from the tunnel TCP connection.
-    var readStream: NSInputStream?
+    var readStream: InputStream?
 
 	/// The stream used to write data to the tunnel TCP connection.
-    var writeStream: NSOutputStream?
+    var writeStream: OutputStream?
 
 	/// A buffer where the data for the current packet is accumulated.
 	let packetBuffer = NSMutableData()
@@ -34,14 +34,14 @@ class ServerTunnel: Tunnel, TunnelDelegate, NSStreamDelegate {
 
 	// MARK: Initializers
 
-	init(newReadStream: NSInputStream, newWriteStream: NSOutputStream) {
+	init(newReadStream: InputStream, newWriteStream: OutputStream) {
 		super.init()
 		delegate = self
 
 		for stream in [newReadStream, newWriteStream] {
 			stream.delegate = self
 			stream.open()
-			stream.scheduleInRunLoop(NSRunLoop.mainRunLoop(), forMode: NSDefaultRunLoopMode)
+			stream.schedule(in: RunLoop.main, forMode: .defaultRunLoopMode)
 		}
 		readStream = newReadStream
 		writeStream = newWriteStream
@@ -50,14 +50,14 @@ class ServerTunnel: Tunnel, TunnelDelegate, NSStreamDelegate {
 	// MARK: Class Methods
 
 	/// Start the network service.
-	class func startListeningOnPort(port: Int32) -> NSNetService {
-		let service = NSNetService(domain:Tunnel.serviceDomain, type:Tunnel.serviceType, name: "", port: port)
+	class func startListeningOnPort(_ port: Int32) -> NetService {
+		let service = NetService(domain:Tunnel.serviceDomain, type:Tunnel.serviceType, name: "", port: port)
 
 		simpleTunnelLog("Starting network service on port \(port)")
 
 		service.delegate = ServerTunnel.serviceDelegate
-		service.publishWithOptions(NSNetServiceOptions.ListenForConnections)
-		service.scheduleInRunLoop(.mainRunLoop(), forMode: NSDefaultRunLoopMode)
+        service.publish(options: .listenForConnections)
+        service.schedule(in: RunLoop.main, forMode: .defaultRunLoopMode)
 
 		return service
 	}
@@ -73,7 +73,7 @@ class ServerTunnel: Tunnel, TunnelDelegate, NSStreamDelegate {
 	func handleBytesAvailable() -> Bool {
 
 		guard let stream = readStream else { return false }
-		var readBuffer = [UInt8](count: Tunnel.maximumMessageSize, repeatedValue: 0)
+        var readBuffer = [UInt8](repeating: 0, count: Tunnel.maximumMessageSize)
 
 		repeat {
 			var toRead = 0
@@ -81,7 +81,7 @@ class ServerTunnel: Tunnel, TunnelDelegate, NSStreamDelegate {
 
 			if packetBytesRemaining == 0 {
 				// Currently reading the total length of the packet.
-				toRead = sizeof(UInt32.self) - packetBuffer.length
+				toRead = MemoryLayout<UInt32>.size - packetBuffer.length
 			}
 			else {
 				// Currently reading the packet payload.
@@ -94,18 +94,18 @@ class ServerTunnel: Tunnel, TunnelDelegate, NSStreamDelegate {
 				return false
 			}
 
-			packetBuffer.appendBytes(readBuffer, length: bytesRead)
+            packetBuffer.append(readBuffer, length: bytesRead)
 
 			if packetBytesRemaining == 0 {
 				// Reading the total length, see if the 4 length bytes have been received.
-				if packetBuffer.length == sizeof(UInt32.self) {
+				if packetBuffer.length == MemoryLayout<UInt32>.size {
 					var totalLength: UInt32 = 0
-					packetBuffer.getBytes(&totalLength, length: sizeofValue(totalLength))
+					packetBuffer.getBytes(&totalLength, length: MemoryLayout.size(ofValue: totalLength))
 
 					guard totalLength <= UInt32(Tunnel.maximumMessageSize) else { return false }
 
 					// Compute the length of the payload.
-					packetBytesRemaining = Int(totalLength) - sizeofValue(totalLength)
+					packetBytesRemaining = Int(totalLength) - MemoryLayout.size(ofValue: totalLength)
 					packetBuffer.length = 0
 				}
 			}
@@ -114,7 +114,7 @@ class ServerTunnel: Tunnel, TunnelDelegate, NSStreamDelegate {
 				packetBytesRemaining -= bytesRead
 				if packetBytesRemaining == 0 {
 					// The entire packet has been received, process it.
-					if !handlePacket(packetBuffer) {
+                    if !handlePacket(packetBuffer as Data) {
 						return false
 					}
 					packetBuffer.length = 0
@@ -126,9 +126,9 @@ class ServerTunnel: Tunnel, TunnelDelegate, NSStreamDelegate {
 	}
 
 	/// Send an "Open Result" message to the client.
-	func sendOpenResultForConnection(connectionIdentifier: Int, resultCode: TunnelConnectionOpenResult) {
-		let properties = createMessagePropertiesForConnection(connectionIdentifier, commandType: .OpenResult, extraProperties:[
-				TunnelMessageKey.ResultCode.rawValue: resultCode.rawValue
+	func sendOpenResultForConnection(_ connectionIdentifier: Int, resultCode: TunnelConnectionOpenResult) {
+		let properties = createMessagePropertiesForConnection(connectionIdentifier, commandType: .openResult, extraProperties:[
+				TunnelMessageKey.ResultCode.rawValue: resultCode.rawValue as AnyObject
 			])
 
 		if !sendMessage(properties) {
@@ -139,99 +139,105 @@ class ServerTunnel: Tunnel, TunnelDelegate, NSStreamDelegate {
 	/// Handle a "Connection Open" message received from the client.
 	func handleConnectionOpen(properties: [String: AnyObject]) {
 		guard let connectionIdentifier = properties[TunnelMessageKey.Identifier.rawValue] as? Int,
-			tunnelLayerNumber = properties[TunnelMessageKey.TunnelType.rawValue] as? Int,
-			tunnelLayer = TunnelLayer(rawValue: tunnelLayerNumber)
+            let tunnelLayerNumber = properties[TunnelMessageKey.TunnelType.rawValue] as? Int,
+			let tunnelLayer = TunnelLayer(rawValue: tunnelLayerNumber)
 			else { return }
 
 		switch tunnelLayer {
-			case .App:
-
-				guard let flowKindNumber = properties[TunnelMessageKey.AppProxyFlowType.rawValue] as? Int,
-					flowKind = AppProxyFlowKind(rawValue: flowKindNumber)
-					else { break }
-
-				switch flowKind {
-					case .TCP:
-						guard let host = properties[TunnelMessageKey.Host.rawValue] as? String,
-							port = properties[TunnelMessageKey.Port.rawValue] as? NSNumber
-							else { break }
-						let newConnection = ServerConnection(connectionIdentifier: connectionIdentifier, parentTunnel: self)
-						guard newConnection.open(host, port: port.integerValue) else {
-							newConnection.closeConnection(.All)
-							break
-						}
-
-					case .UDP:
-						let _ = UDPServerConnection(connectionIdentifier: connectionIdentifier, parentTunnel: self)
-						sendOpenResultForConnection(connectionIdentifier, resultCode: .Success)
-				}
-
-			case .IP:
-				let newConnection = ServerTunnelConnection(connectionIdentifier: connectionIdentifier, parentTunnel: self)
-				guard newConnection.open() else {
-					newConnection.closeConnection(.All)
-					break
-				}
+			case .app:
+                guard let flowKindNumber = properties[TunnelMessageKey.AppProxyFlowType.rawValue] as? Int,
+                    let flowKind = AppProxyFlowKind(rawValue: flowKindNumber)
+                    else { break }
+                
+                switch flowKind {
+                case .tcp:
+                    guard let host = properties[TunnelMessageKey.Host.rawValue] as? String,
+                        let port = properties[TunnelMessageKey.Port.rawValue] as? NSNumber
+                        else { break }
+                    let newConnection = ServerConnection(connectionIdentifier: connectionIdentifier, parentTunnel: self)
+                    guard newConnection.open(host: host, port: port.intValue) else {
+                        newConnection.closeConnection(.all)
+                        break
+                    }
+                    
+                case .udp:
+                    let _ = UDPServerConnection(connectionIdentifier: connectionIdentifier, parentTunnel: self)
+                    sendOpenResultForConnection(connectionIdentifier, resultCode: .success)
+                }
+            
+			case .ip:
+                let newConnection = ServerTunnelConnection(connectionIdentifier: connectionIdentifier, parentTunnel: self)
+                guard newConnection.open() else {
+                    newConnection.closeConnection(.all)
+                    break
+                }
 		}
 	}
 
-	// MARK: NSStreamDelegate
+	// MARK: StreamDelegate
 
-	/// Handle a stream event.
-    func stream(aStream: Stream, handleEvent eventCode: StreamEvent) {
-		switch aStream {
-
-			case writeStream!:
-				switch eventCode {
-					case [.HasSpaceAvailable]:
-						// Send any buffered data.
-						if !savedData.isEmpty {
-							guard savedData.writeToStream(writeStream!) else {
-								closeTunnel()
-								delegate?.tunnelDidClose(self)
-								break
-							}
-
-							if savedData.isEmpty {
-								for connection in connections.values {
-									connection.resume()
-								}
-							}
-						}
-
-					case [.ErrorOccurred]:
-						closeTunnel()
-						delegate?.tunnelDidClose(self)
-
-					default:
-						break
-				}
-
-			case readStream!:
-				var needCloseTunnel = false
-				switch eventCode {
-					case [.HasBytesAvailable]:
-						needCloseTunnel = !handleBytesAvailable()
-
-					case [.OpenCompleted]:
-						delegate?.tunnelDidOpen(self)
-
-					case [.ErrorOccurred], [.EndEncountered]:
-						needCloseTunnel = true
-
-					default:
-						break
-				}
-
-				if needCloseTunnel {
-					closeTunnel()
-					delegate?.tunnelDidClose(self)
-				}
-
-			default:
-				break
+    /// Handle a stream event.
+    func stream(_ aStream: Stream, handle eventCode: Stream.Event) {
+  
+        switch aStream {
+        case writeStream!:
+            handleInputStreamEvent(eventCode)
+        case readStream!:
+            handleOutputStreamEvent(eventCode)
+        default:
+            break
         }
-
+    }
+    
+    /// Handle an InputStream event.
+    func handleInputStreamEvent(_ eventCode: Stream.Event) {
+        var needCloseTunnel = false
+        switch eventCode {
+        case [.hasBytesAvailable]:
+            needCloseTunnel = !handleBytesAvailable()
+            
+        case [.openCompleted]:
+            delegate?.tunnelDidOpen(self)
+        
+        case [.errorOccurred]:
+            needCloseTunnel = true
+            
+        default:
+            break
+        }
+        
+        if needCloseTunnel {
+            closeTunnel();
+            delegate?.tunnelDidClose(self)
+        }
+    }
+    
+    /// Handle an OutputStream event.
+    func handleOutputStreamEvent(_ eventCode: Stream.Event) {
+        switch eventCode {
+        case [.hasSpaceAvailable]:
+            // Send any buffered data.
+            if !savedData.isEmpty {
+                guard savedData.writeToStream(writeStream!) else {
+                    closeTunnel()
+                    delegate?.tunnelDidClose(self)
+                    break
+                }
+                
+                if savedData.isEmpty {
+                    for connection in connections.values {
+                        connection.resume()
+                    }
+                }
+            }
+        
+        case [.errorOccurred]:
+            closeTunnel()
+            delegate?.tunnelDidClose(self)
+            
+        default:
+            break
+        }
     }
 
 	// MARK: Tunnel
@@ -244,16 +250,16 @@ class ServerTunnel: Tunnel, TunnelDelegate, NSStreamDelegate {
                 simpleTunnelLog("Tunnel read stream error: \(error)")
             }
 
-			let socketData = CFReadStreamCopyProperty(stream, kCFStreamPropertySocketNativeHandle) as? NSData
-
-            stream.removeFromRunLoop(NSRunLoop.mainRunLoop(), forMode: NSDefaultRunLoopMode)
+            let socketData = CFReadStreamCopyProperty(stream, CFStreamPropertyKey.socketNativeHandle) as? NSData
+            
+            stream.remove(from: RunLoop.main, forMode: .defaultRunLoopMode)
             stream.close()
             stream.delegate = nil
             readStream = nil
 
 			if let data = socketData {
 				var socket: CFSocketNativeHandle = 0
-				data.getBytes(&socket, length: sizeofValue(socket))
+				data.getBytes(&socket, length: MemoryLayout.size(ofValue: socket))
 				close(socket)
 			}
         }
@@ -263,7 +269,7 @@ class ServerTunnel: Tunnel, TunnelDelegate, NSStreamDelegate {
                 simpleTunnelLog("Tunnel write stream error: \(error)")
             }
 
-            stream.removeFromRunLoop(.mainRunLoop(), forMode: NSDefaultRunLoopMode)
+            stream.remove(from: RunLoop.main, forMode: .defaultRunLoopMode)
             stream.close()
             stream.delegate = nil
         }
@@ -272,16 +278,17 @@ class ServerTunnel: Tunnel, TunnelDelegate, NSStreamDelegate {
     }
 
 	/// Handle a message received from the client.
-	override func handleMessage(commandType: TunnelCommand, properties: [String: AnyObject], connection: Connection?) -> Bool {
+	override func handleMessage(_ commandType: TunnelCommand, properties: [String: AnyObject], connection: Connection?) -> Bool {
 		switch commandType {
-			case .Open:
-				handleConnectionOpen(properties)
+			case .open:
+                handleConnectionOpen(properties: properties)
 
-			case .FetchConfiguration:
+			case .fetchConfiguration:
 				var personalized = ServerTunnel.configuration.configuration
-				personalized.removeValueForKey(SettingsKey.IPv4.rawValue)
-				let messageProperties = createMessagePropertiesForConnection(0, commandType: .FetchConfiguration, extraProperties: [TunnelMessageKey.Configuration.rawValue: personalized])
-				sendMessage(messageProperties)
+                personalized.removeValue(forKey: SettingsKey.IPv4.rawValue)
+				let messageProperties = createMessagePropertiesForConnection(0, commandType: .fetchConfiguration, extraProperties: [
+                    TunnelMessageKey.Configuration.rawValue: personalized as AnyObject])
+				_ = sendMessage(messageProperties)
 
 			default:
 				break
@@ -290,7 +297,7 @@ class ServerTunnel: Tunnel, TunnelDelegate, NSStreamDelegate {
 	}
 
 	/// Write data to the tunnel connection.
-    override func writeDataToTunnel(data: NSData, startingAtOffset: Int) -> Int {
+    override func writeDataToTunnel(_ data: Data, startingAtOffset: Int) -> Int {
 		guard let stream = writeStream else { return -1 }
 		return writeData(data, toStream: stream, startingAtOffset:startingAtOffset)
     }
@@ -298,44 +305,44 @@ class ServerTunnel: Tunnel, TunnelDelegate, NSStreamDelegate {
 	// MARK: TunnelDelegate
 
 	/// Handle the "tunnel open" event.
-	func tunnelDidOpen(targetTunnel: Tunnel) {
+	func tunnelDidOpen(_ targetTunnel: Tunnel) {
 	}
 
 	/// Handle the "tunnel closed" event.
-	func tunnelDidClose(targetTunnel: Tunnel) {
+	func tunnelDidClose(_ targetTunnel: Tunnel) {
 	}
 
 	/// Handle the "tunnel did send configuration" event.
-	func tunnelDidSendConfiguration(targetTunnel: Tunnel, configuration: [String : AnyObject]) {
+	func tunnelDidSendConfiguration(_ targetTunnel: Tunnel, configuration: [String : AnyObject]) {
 	}
 }
 
 /// An object that servers as the delegate for the network service published by the server.
-class ServerDelegate : NSObject, NSNetServiceDelegate {
+class ServerDelegate : NSObject, NetServiceDelegate {
 
-	// MARK: NSNetServiceDelegate
+	// MARK: NetServiceDelegate
 
-	/// Handle the "failed to publish" event.
-	func netService(sender: NSNetService, didNotPublish errorDict: [String : NSNumber]) {
-		simpleTunnelLog("Failed to publish network service")
-		exit(1)
-	}
+    /// Handle the "failed to publish" event.
+    func netService(_ sender: NetService, didNotPublish errorDict: [String : NSNumber]) {
+        simpleTunnelLog("Failed to publish network service")
+        exit(EXIT_FAILURE)
+    }
+    
+    /// Handle the "published" event.
+    func netServiceDidPublish(_ sender: NetService) {
+        simpleTunnelLog("Network service published successfully")
+    }
 
-	/// Handle the "published" event.
-	func netServiceDidPublish(sender: NSNetService) {
-		simpleTunnelLog("Network service published successfully")
-	}
+    /// Handle the "new connection" event.
+    func netService(_ sender: NetService, didAcceptConnectionWith inputStream: InputStream, outputStream: OutputStream) {
+        simpleTunnelLog("Accepted a new connection")
+        _ = ServerTunnel(newReadStream: inputStream, newWriteStream: outputStream)
+    }
 
-	/// Handle the "new connection" event.
-	func netService(sender: NSNetService, didAcceptConnectionWithInputStream inputStream: NSInputStream, outputStream: NSOutputStream) {
-		simpleTunnelLog("Accepted a new connection")
-		_ = ServerTunnel(newReadStream: inputStream, newWriteStream: outputStream)
-	}
-
-	/// Handle the "stopped" event.
-	func netServiceDidStop(sender: NSNetService) {
-		simpleTunnelLog("Network service stopped")
-		exit(0)
-	}
+    /// Handle the "stopped" event.
+    func netServiceDidStop(_ sender: NetService) {
+        simpleTunnelLog("Network service stopped")
+        exit(EXIT_SUCCESS)
+    }
 }
 
